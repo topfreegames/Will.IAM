19acd01c2e675dfe5a1b2a4591a50983658df470
Gerson Silva <gerson.silva@wildlifestudios.com>
2019-10-07 15:47:16 -0300

    fixup! Improves checking existence of tags in Docker Hub

--- push_to_docker.sh
+++ push_to_docker.sh
@@ -5,6 +5,7 @@ set -euo pipefail
 # The image build step occurs only when a build is triggered by a commit being merged into the "master" branch.
 # Given that the "master" branch is protected, the only way to trigger a build from the "master" branch is when a PR
 # is merged into it. This way, we avoid storing images from non-stable branches.
+DOCKER_HUB_REPO="tfgco/will-iam"
 LAST_COMMIT_SHA=$(git rev-parse --short HEAD)
 VERSION=$(cat version.txt)
 
@@ -12,7 +13,7 @@ function docker_tag_exists() {
     curl --silent -f -lSL "https://index.docker.io/v1/repositories/$1/tags/$2" > /dev/null
 }
 
-if docker_tag_exists tfgco/will-iam "$VERSION"; then
+if docker_tag_exists "$DOCKER_HUB_REPO" "$VERSION"; then
     echo "An image with the version $VERSION already exists in Docker Hub. Please update your version.txt file and try again."
     exit 1
 fi
@@ -20,9 +21,9 @@ fi
 docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
 
 docker build -t will-iam .
-docker tag will-iam:latest tfgco/will-iam:"$LAST_COMMIT_SHA"
-docker tag will-iam:latest tfgco/will-iam:"$VERSION"
-docker tag will-iam:latest tfgco/will-iam:latest
-docker push tfgco/will-iam:"$LAST_COMMIT_SHA"
-docker push tfgco/will-iam:"$VERSION"
-docker push tfgco/will-iam:latest
+docker tag will-iam "$DOCKER_HUB_REPO":"$LAST_COMMIT_SHA"
+docker tag will-iam "$DOCKER_HUB_REPO":"$VERSION"
+docker tag will-iam "$DOCKER_HUB_REPO":latest
+docker push "$DOCKER_HUB_REPO":"$LAST_COMMIT_SHA"
+docker push "$DOCKER_HUB_REPO":"$VERSION"
+docker push "$DOCKER_HUB_REPO":latest
