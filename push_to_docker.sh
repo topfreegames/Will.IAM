372c5afee493d03f2a07f39b6f912b3ff3594331
Gerson Silva <gerson.silva@wildlifestudios.com>
2019-10-07 18:50:07 -0300

    Improve syntax in push_to_docker.sh

--- push_to_docker.sh
+++ push_to_docker.sh
@@ -5,23 +5,31 @@ set -euo pipefail
 # The image build step occurs only when a build is triggered by a commit being merged into the "master" branch.
 # Given that the "master" branch is protected, the only way to trigger a build from the "master" branch is when a PR
 # is merged into it. This way, we avoid storing images from non-stable branches.
-DOCKER_HUB_REPO="tfgco/will-iam"
-LAST_COMMIT_SHA=$(git rev-parse --short HEAD)
-VERSION=$(cat version.txt)
+DOCKER_HUB_REPO='tfgco/will-iam'
 
-function docker_tag_exists() {
-    curl --silent -f -lSL "https://index.docker.io/v1/repositories/$1/tags/$2" > /dev/null
+docker_tag_exists() {
+    local repo="$1"
+    local tag="$2"
+    curl --silent -flSL "https://index.docker.io/v1/repositories/$repo/tags/$tag" > /dev/null
 }
 
-if docker_tag_exists "$DOCKER_HUB_REPO" "$VERSION"; then
-    echo "An image with the version $VERSION already exists in Docker Hub. Please update your version.txt file and try again."
-    exit 1
-fi
+main() {
+  local last_commit_sha
+  local version
+  last_commit_sha=$(git rev-parse --short HEAD)
+  version=$(cat version.txt)
 
-docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
+  if docker_tag_exists "$DOCKER_HUB_REPO" "$version"; then
+      echo "An image with the version $version already exists in Docker Hub. Please update your version.txt file and try again."
+      exit 1
+  fi
 
-docker build -t will-iam .
-docker tag will-iam "$DOCKER_HUB_REPO":"$LAST_COMMIT_SHA"
-docker tag will-iam "$DOCKER_HUB_REPO":"$VERSION"
-docker push "$DOCKER_HUB_REPO":"$LAST_COMMIT_SHA"
-docker push "$DOCKER_HUB_REPO":"$VERSION"
+  docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
+  docker build -t will-iam .
+  docker tag will-iam "$DOCKER_HUB_REPO:$last_commit_sha"
+  docker tag will-iam "$DOCKER_HUB_REPO:$version"
+  docker push "$DOCKER_HUB_REPO:$last_commit_sha"
+  docker push "$DOCKER_HUB_REPO:$version"
+}
+
+main "$@"
